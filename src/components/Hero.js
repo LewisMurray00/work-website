5fda2f9a29fedde688bedd45bde9af22f1c25674
Lewis Murray <lewisdmurray00@gmail.com>
2021-11-04 20:05:22 +0000

    Added all the styled components that are needed to create the image slider bar, now styling

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled, { css } from 'styled-components/macro'
 import { NavButton } from './NavButton';
+import {IoMdArrowRoundForward} from 'react-icons/io'
+import {IoArrowForward, IoArrowBack} from 'react-icons/io5'
+
 
 //Styled components:
 const HeroSection = styled.section`
@@ -24,7 +27,25 @@ const HeroSlide = styled.div``;
 const HeroSlider = styled.div``;
 const HeroImage = styled.img``;
 const HeroContent = styled.div``;
+const Arrow = styled(IoMdArrowRoundForward)``;
 
+const arrowButtons = css`
+    width: 50px;
+    height: 50px;
+    color: #fff;
+    cursor: pointer;
+    background: #000d1a;
+    border-radius: 50px;
+    padding: 10px;
+    margin-right: 1rem;
+    user-select: none;
+    transition: 0.3s;
+
+    &:hover{
+        background: blue;
+        transform: scale(1.05);
+    }
+`;
 
 
 //Actual component
@@ -39,14 +60,19 @@ const Hero = ({slides}) => {
                                 <HeroImage />
                                 <HeroContent>
                                     <h1>{slide.title}</h1>
-                                    <NavButton to={slide.path} primary="true">
+                                    <NavButton to={slide.path} primary="true" css={`max-width:160px`}>
                                         {slide.label}
+                                        <Arrow />
                                     </NavButton>
                                 </HeroContent>
                             </HeroSlider>
                         </HeroSlide>
                     )
                 })}
+                <SliderButtons>
+                    <PrevArrow />
+                    <NextArrow />
+                </SliderButtons>
             </HeroWrapper>
         </HeroSection>
     )
